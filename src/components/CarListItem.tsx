bded9146ae833533c8c7a4e3d0b1978d645c0738
JereUR <jeremias.jdv@gmail.com>
2024-12-12 22:27:18 -0300

    feat: List car item component added

---
+++ src/components/CarListItem.tsx
@@ -0,0 +1,10 @@
+import { CarData } from "@/types/api"
+
+interface CarListItemProps {
+  car: CarData
+}
+export default function CarListItem({ car }: CarListItemProps) {
+  return (
+    <div>CarItem</div>
+  )
+}
