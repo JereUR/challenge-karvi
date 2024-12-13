19d6631706216ae077c3c3667bdab83033ed8480
JereUR <jeremias.jdv@gmail.com>
2024-12-12 22:25:59 -0300

    feat: Project initialized

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,25 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn', // Cambia a "off" para deshabilitar completamente la regla
+        { vars: 'all', args: 'after-used', ignoreRestSiblings: true }
+      ],
+      semi: ['error', 'never']
+    }
+  }
+]
 
-export default eslintConfig;
+export default eslintConfig
