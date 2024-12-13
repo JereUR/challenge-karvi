341830eb76f398f4c949627c4b5c5bb079ec9cc0
JereUR <jeremias.jdv@gmail.com>
2024-12-13 01:39:58 -0300

    feat: Suspense added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 "use client"
 
 import ListCars from "@/components/ListCars"
-import { useState } from "react"
+import { Suspense, useState } from "react"
 
 export default function Home() {
   const [gridMode, setGridMode] = useState<boolean>(true)
 
   return (
     <div className="mx-auto flex gap-4">
-      <ListCars gridMode={gridMode} />
+      <Suspense>
+        <ListCars gridMode={gridMode} />
+      </Suspense>
     </div>
   )
 }
