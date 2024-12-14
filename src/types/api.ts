e8924ce8f596eb5650e5e2e303a0f32267a9e71c
JereUR <jeremias.jdv@gmail.com>
2024-12-14 03:58:03 -0300

    feat: Types for filters added

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,10 +1,35 @@
 export interface CarData {
-  id: string;
-  city: string;
-  year: number;
-  brand: string;
-  model: string;
-  version: string;
-  price: number;
-  mileage: number;
+  id: string
+  city: string
+  year: number
+  brand: string
+  model: string
+  version: string
+  price: number
+  mileage: number
+}
+
+export interface FilterBrandYearVersion {
+  id: string
+  name: string
+}
+
+export interface FilterCity {
+  id: string
+  name: string
+  slug: string
+}
+
+export interface FilterModel {
+  id: string
+  name: string
+  brand: string
+}
+
+export interface AllFilters {
+  brands: FilterBrandYearVersion[]
+  cities: FilterCity[]
+  models: FilterModel[]
+  years: FilterBrandYearVersion[]
+  versions: FilterBrandYearVersion[]
 }
