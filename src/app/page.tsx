a797b05064bff7f9549ce2efac92492990f14b1d
JereUR <jeremias.jdv@gmail.com>
2024-12-14 03:51:50 -0300

    feat: Filter bar/icon moved to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,28 @@ import { Suspense } from "react"
 
 import ListCars from "@/components/ListCars"
 import { useGridMode } from "@/hooks/useGridMode"
+import { FilterSelector } from "@/components/FilterSelector"
 
 export default function Home() {
   const [gridMode, setGridMode] = useGridMode()
 
   return (
-    <div className="mx-auto flex flex-col gap-4">
-      <Suspense>
-        <ListCars gridMode={gridMode} setGridMode={setGridMode} />
-      </Suspense>
-    </div>
+    <main className="flex min-h-screen flex-col">
+      <div className="flex w-full justify-around border-t bg-card py-3 sm:hidden z-50">
+        Icon filter bar
+      </div>
+      <div className="flex w-full grow md:gap-5 p-2 md:p-5">
+        <div className="sticky top-[8.25rem] flex flex-col gap-5">
+          <div className="hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80 z-50">
+            <FilterSelector />
+          </div>
+        </div>
+        <div className="mx-auto flex flex-col gap-4">
+          <Suspense>
+            <ListCars gridMode={gridMode} setGridMode={setGridMode} />
+          </Suspense>
+        </div>
+      </div>
+    </main>
   )
 }
