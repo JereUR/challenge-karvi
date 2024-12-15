fb71334ce9c6a6b84f27f73beba65af9b46d245b
JereUR <jeremias.jdv@gmail.com>
2024-12-14 21:09:40 -0300

    feat: Component for select filters icon responsive added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,30 @@ import { Suspense } from "react"
 import ListCars from "@/components/ListCars"
 import { useGridMode } from "@/hooks/useGridMode"
 import { FilterSelector } from "@/components/FilterSelector"
+import { ActiveFilters } from "@/components/ActiveFilters"
+import Filtericon from "@/components/FilterIcon"
+import SearchIcon from "@/components/SearchIcon"
 
 export default function Home() {
   const [gridMode, setGridMode] = useGridMode()
 
   return (
-    <main className="flex min-h-screen flex-col">
-      <div className="flex w-full justify-around border-t bg-card py-3 sm:hidden z-50">
-        Icon filter bar
+    <main className="flex min-h-screen flex-col space-y-2 md:space-y-3">
+      <div className="flex w-full justify-around py-1 sm:hidden z-50">
+        <SearchIcon />
+        <div className="h-6 my-auto w-px bg-border" aria-hidden="true" />
+        <Filtericon />
       </div>
       <div className="flex w-full grow md:gap-5 p-2 md:p-5">
         <div className="sticky top-[8.25rem] flex flex-col gap-5">
-          <div className="hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80 z-50">
+          <div className="hidden h-fit flex-none space-y-3 px-3 py-5 sm:block lg:px-5 xl:w-80 z-50">
             <Suspense>
               <FilterSelector />
             </Suspense>
           </div>
         </div>
         <div className="mx-auto flex flex-col gap-4">
+          <ActiveFilters />
           <Suspense>
             <ListCars gridMode={gridMode} setGridMode={setGridMode} />
           </Suspense>
