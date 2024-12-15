cf164c7dd584c070c496f4e06161f45a461ccf99
JereUR <jeremias.jdv@gmail.com>
2024-12-14 21:10:13 -0300

    feat: Search icon added

---
+++ src/components/SearchIcon.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Search } from 'lucide-react'
+
+import { Button } from './ui/button'
+
+export default function SearchIcon() {
+  return (
+    <div><Button variant="ghost" className="flex items-center gap-2 text-primary font-bold">
+      <Search className="h-5 w-5" />
+      Buscar
+    </Button></div>
+  )
+}
