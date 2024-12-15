2cd836a9ed12aa45c7ce3fb5c178c097dfdf924f
JereUR <jeremias.jdv@gmail.com>
2024-12-14 21:09:35 -0300

    feat: Component for select filters icon responsive added

---
+++ src/components/FilterIcon.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { useState } from "react"
+import { SlidersVertical } from "lucide-react"
+
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import { Button } from "./ui/button"
+import { FilterSelector } from "@/components/FilterSelector"
+
+export default function Filtericon() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleDialog = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <div>
+      <Button variant='ghost' onClick={toggleDialog} className="flex items-center gap-2 text-primary font-bold">
+        <SlidersVertical />Filtrar
+      </Button>
+      <Dialog open={isOpen} onOpenChange={toggleDialog}>
+        <DialogContent className="max-w-lg p-6 overflow-auto max-h-[80vh]">
+          <DialogHeader className="flex justify-between">
+            <DialogTitle className="text-xl font-semibold">Selecciona Filtros</DialogTitle>
+          </DialogHeader>
+          <div className="overflow-y-auto max-h-[60vh]">
+            <FilterSelector />
+          </div>
+        </DialogContent>
+      </Dialog>
+    </div>
+  )
+}
